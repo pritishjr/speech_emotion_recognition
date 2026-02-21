0fbc399937cdb9a2f9ccbedfa71e4e01168cf3c9
pritishjr <pritishjr.personal@gmail.com>
2026-02-22 02:44:57 +0530

    creating the video model

---
+++ src/models/video_model.py
@@ -0,0 +1,66 @@
+
+'''
+input shape:    (batch, vid_frames, channels, height, width) #channel first!
+output shape:   sequence/timeline: (batch, seq length, 768)
+                pooled: (batch, 768)
+                
+'''
+
+import torch
+import torch.nn as nn
+from transformers import VivitModel
+from transformers import logging
+
+class VideoFeatureExtractor(nn.Module):
+    
+    def __init__(self, pretrained_model='google/vivit-b-16x2-kinetics400', freeze_layers = int):
+        super(VideoFeatureExtractor, self).__init__()
+        
+        #initializing the vivit model
+        self.vivit = VivitModel.from_pretrained(pretrained_model)
+        
+        #embeddings chop up the frames into tubeletes(3D)
+        for param in self.vivit.embeddings.parameters():
+            param.requires_grad=False #no updating/backprop the params
+                
+        if freeze_layers>0:
+            #freezing the last 8 layers' params
+            for param in self.vivit.encoder.layer[:freeze_layers].parameters():
+                param.requires_grad = False
+        
+    def forward(self, vals):
+        
+        #we have two outputs given for the input.
+        outputs = self.vivit(vals)
+        
+        #sequence/timeline: the last hidden captures this, important for the cross attention fusion of modalities.
+        sequence_output = outputs.last_hidden_state
+        
+        #pooled
+        pooled_output = outputs.pooler_output
+        
+        return sequence_output, pooled_output
+    
+def main():
+
+    #taking only one batch, rest the same.
+    example = torch.randn(1, 32, 224, 224, 3) #channel first shape.
+    
+    #loading the vivit pretrained model:
+    logging.set_verbosity_info()
+    print("Loading the ViViT model.")
+    model = VideoFeatureExtractor(freeze_layers=8)
+    
+    print("giving output for the example.")
+    seq_out , pool_out = model(example)
+    
+    print(f"""
+          Checking the shapes:
+          input tensor shape (4D) : {example.shape}
+          seq_out shape: {seq_out.shape}
+          pool_out shape: {pool_out.shape}
+          """)
+    pass
+
+if __name__ == "__main__":
+    main()
